5093bcde70f27680e8492a1160784633afa1cd85
Soumya Sagar <soumyasagar81@gmail.com>
2025-07-11 01:47:23 +0000

    Hey first stop the login page and sign up page

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,27 +3,27 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get('firebaseIdToken');
-  const { pathname } = request.nextUrl;
+  // const token = request.cookies.get('firebaseIdToken');
+  // const { pathname } = request.nextUrl;
 
-  const protectedRoutes = ['/new-story', '/messages', '/teams', '/settings'];
+  // const protectedRoutes = ['/new-story', '/messages', '/teams', '/settings'];
 
-  const isProtectedRoute = protectedRoutes.some(route => pathname.startsWith(route));
+  // const isProtectedRoute = protectedRoutes.some(route => pathname.startsWith(route));
 
-  if (isProtectedRoute && !token) {
-    const url = request.nextUrl.clone()
-    url.pathname = '/login'
-    return NextResponse.redirect(url)
-  }
+  // if (isProtectedRoute && !token) {
+  //   const url = request.nextUrl.clone()
+  //   url.pathname = '/login'
+  //   return NextResponse.redirect(url)
+  // }
 
   return NextResponse.next()
 }
 
 export const config = {
   matcher: [
-    '/new-story/:path*',
-    '/messages/:path*',
-    '/teams/:path*',
-    '/settings/:path*',
+    // '/new-story/:path*',
+    // '/messages/:path*',
+    // '/teams/:path*',
+    // '/settings/:path*',
   ],
 }
